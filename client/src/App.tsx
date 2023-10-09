16673e761ed5b6a7052a010313087b3746edf0ff
John <ashton.basile@gmail.com>
2023-10-09 14:26:54 +0100

    the websocket between client and server works somewhat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
-// import WebSocketClient from './services/WebSocketService';
+import WebSocketClient from './services/WebSocketService';
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
-      {/* <WebSocketClient /> */}
+      <WebSocketClient />
     </>
   );
 }
