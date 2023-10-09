84c0b58091afb5c7a3321385987920f91e7afb3d
John <ashton.basile@gmail.com>
2023-10-09 16:32:21 +0100

    added the /deck route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import SoloGame from './components/SoloGame';
 import PVPGame from './components/PVPGame';
+import DeckBuilder from './components/DeckBuilder';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/solo" element={<SoloGame />} />
           <Route path="/pvp" element={<PVPGame />} />
+          <Route path="/deck" element={<DeckBuilder />} />
         </Routes>
       </Router>
     </>
