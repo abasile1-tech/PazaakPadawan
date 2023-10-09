5c269f0c14aa6d3107ab86d1dd96c699e4e3c226
John <ashton.basile@gmail.com>
2023-10-09 16:21:42 +0100

    added routing for /, /pvp, and /solo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
 // import WebSocketClient from './services/WebSocketService';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SoloGame from './components/SoloGame';
+import PVPGame from './components/PVPGame';
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
       {/* <WebSocketClient /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/solo" element={<SoloGame />} />
+          <Route path="/pvp" element={<PVPGame />} />
+        </Routes>
+      </Router>
     </>
   );
 }
