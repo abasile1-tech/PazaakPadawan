baf796f4b770520d014c3fdbe505e900664f58d3
Ewelina Nicholson <e.guzikk@gmail.com>
2023-10-10 09:47:35 +0100

    commented out css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomePage from './HomePage';
 // import WebSocketClient from './services/WebSocketService';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Chat from './components/Chat';
 
 function App() {
-  return <>{/* <WebSocketClient /> */}</>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      {/* <WebSocketClient /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Chat" element={<Chat />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
