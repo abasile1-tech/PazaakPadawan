7499ec543e119a397e219c673d653fc6df695bbf
PaulJD91 <paul.dennehy@hotmail.com>
2023-10-11 11:44:41 +0100

    changed music button to mute and volume icons instead of text buttons

--- client/src/components/BackgroundMusic.tsx
+++ client/src/components/BackgroundMusic.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react';
 import openingTheme from '../assets/music/8bitopeningtheme.mp3';
 import cantina from '../assets/music/8bitcantina.mp3';
+import mute from '../assets/images/icons/Mute.png';
+import voice from '../assets/images/icons/Voice.png';
 
 interface backgroundMusicProps {
   musicChoice: string;
@@ -32,9 +34,7 @@ const BackgroundMusic: React.FC<backgroundMusicProps> = ({ musicChoice }) => {
   return (
     <div>
       <audio ref={audioRef} src={backgroundMusic} loop />
-      <button onClick={togglePlay}>
-        {isPlaying ? 'Pause music' : 'Play music'}
-      </button>
+      <img onClick={togglePlay} src={isPlaying ? mute : voice} />
     </div>
   );
 };
