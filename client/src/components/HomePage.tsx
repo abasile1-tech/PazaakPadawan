19c7ead2e685341d03b40759a5e4068fe861deea
Ewelina Nicholson <e.guzikk@gmail.com>
2023-10-11 20:25:08 +0100

    TIC-33implementUserBarHomeComponent

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -2,14 +2,30 @@ import BackgroundMusic from './BackgroundMusic';
 import Header from './Header';
 import { Link } from 'react-router-dom';
 // import User from './User';
+import { useEffect, useState } from 'react';
 
 function HomePage() {
   const musicChoice: string = 'homePage';
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
+  useEffect(() => {
+    const storedCharacter = localStorage.getItem('selectedCharacter');
+    if (storedCharacter) {
+      setSelectedCharacter(JSON.parse(storedCharacter));
+    }
+  }, []);
   return (
     <div className="home-page">
       <Header />
-      <h2>User Bar Home Component</h2>
+      <h2>
+        {selectedCharacter && (
+          <div className="user-bar">
+            <h3>You chose to be:</h3>
+            <img src={selectedCharacter.image} alt={selectedCharacter.name} />
+            <p>{selectedCharacter.name}</p>
+          </div>
+        )}
+      </h2>
 
       <div className="main-content">
         <div id="home-page-text">
@@ -21,6 +37,7 @@ function HomePage() {
             game in which the goal was to come closest to 20 without going over.
           </h2>
         </div>
+
         <div>
           <img
             id="image-three-cards"
