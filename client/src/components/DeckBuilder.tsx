047a5834f0548d3a1126f6de772e0c6543c69eb8
ying <huaiy1105@gmail.com>
2023-10-12 16:36:16 +0100

    fix popup message

--- client/src/components/DeckBuilder.tsx
+++ client/src/components/DeckBuilder.tsx
@@ -3,13 +3,14 @@ import PopUp from './PopUP/PopUp';
 
 function DeckBuilder() {
   const musicChoice = 'deckBuilder';
-  const popupType = 'welcome';
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {};
+  //set up game turn + 1
   return (
     <>
       <Header musicChoice={musicChoice} />
       <h1>Deck Builder!</h1>
       {/* <PopUp popupType="welcome" /> */}
-      <PopUp popupType={popupType} onClick={onClick} />
+      <PopUp onClick={handleClick} message="THE OPPONENT WINS THE SET." />
     </>
   );
 }
--- client/src/components/PopUP/PopUp.tsx
+++ client/src/components/PopUP/PopUp.tsx
@@ -3,43 +3,21 @@
 import Title from './Title';
 import Message from './Message';
 
-interface gameStatusProps {
-  popupType: string;
-}
 interface PopUpProps {
-  // title?: string;
-  // message: string;
-  // buttonText: string;
+  title?: string;
+  message: string;
+  buttonText?: string;
   onClick: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
-function copyForTutorial(popupType: string) {
-  if (popupType === 'welcome') {
-    return {
-      title: 'WELCOME TO PAZAAK',
-      message:
-        'Beat your opponent by getting closest to 20 without going over.',
-      buttonText: "LET'S GO!",
-    };
-  } else if (popupType === 'result_opponentwins') {
-    return { message: 'THE OPPONENT WINS THE SET.', buttonText: 'OK' };
-  } else if (popupType === 'result_userwins') {
-    return { message: 'YOU WIN THE SET.', buttonText: 'OK' };
-  } else if (popupType === 'result_tie') {
-    return { message: 'THE SET IS TIED.', buttonText: 'OK' };
-  }
-
-  return { title: '', message: '', buttonText: '' };
-}
-
-function PopUp({ popupType, onClick }: gameStatusProps & PopUpProps) {
-  const popupTexts = copyForTutorial(popupType);
+function PopUp({ title, onClick, message, buttonText = 'OK' }: PopUpProps) {
+  // const popupTexts = copyForTutorial(popupType);
   return (
     <div className="popup-message">
-      <Title>{popupTexts.title}</Title>
-      <Message>{popupTexts.message}</Message>
+      {title && <Title>{title}</Title>}
+      <Message>{message}</Message>
       <button onClick={onClick} className="pop-message-button">
-        {popupTexts.buttonText}
+        {buttonText}
       </button>
     </div>
   );
