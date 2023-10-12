760fd6d49ae2464e7e78c169e939f5dbbfc6ec84
PaulJD91 <paul.dennehy@hotmail.com>
2023-10-12 16:43:24 +0100

    hard coded jarjar as the opponent in solo

--- client/src/components/PlayBar.tsx
+++ client/src/components/PlayBar.tsx
@@ -2,29 +2,36 @@ import { useEffect, useState } from 'react';
 import ScoreKeeper from './ScoreKeeper';
 interface PlayBarProps {
   playerTally: number;
+  identity: string;
 }
 
-const PlayBar = ({ playerTally }: PlayBarProps) => {
+const PlayBar = ({ playerTally, identity }: PlayBarProps) => {
   const [selectedCharacter, setSelectedCharacter] = useState(null);
 
-  useEffect(() => {
-    const storedCharacter = localStorage.getItem('selectedCharacter');
-    if (storedCharacter) {
-      setSelectedCharacter(JSON.parse(storedCharacter));
-    }
-  }, []);
+  if (identity == 'player') {
+    useEffect(() => {
+      const storedCharacter = localStorage.getItem('selectedCharacter');
+      if (storedCharacter) {
+        setSelectedCharacter(JSON.parse(storedCharacter));
+      }
+    }, []);
+  }
 
   return (
     <>
       <h2>
-        {selectedCharacter ? (
-          <div className="user-bar">
-            <img src={selectedCharacter.image} />
-            <h3>{selectedCharacter.name}!</h3>
-            <ScoreKeeper cardTally={playerTally} />
-          </div>
+        {identity === 'player' ? (
+          selectedCharacter ? (
+            <div className="user-bar">
+              <img src={selectedCharacter.image} alt={selectedCharacter.name} />
+              <h3>{selectedCharacter.name}!</h3>
+              <ScoreKeeper cardTally={playerTally} />
+            </div>
+          ) : (
+            <p>Character is not chosen</p>
+          )
         ) : (
-          <p>Character is not chosen</p>
+          <h1>jarjar</h1>
         )}
       </h2>
     </>
