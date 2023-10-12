e1ebe5fc62e1477a18a443ce1d9f01b591eacc9d
John <ashton.basile@gmail.com>
2023-10-12 10:46:55 +0100

    (TIC-58-createHandComponent)

--- client/src/components/Hand.tsx
+++ client/src/components/Hand.tsx
@@ -1,28 +1,23 @@
-import { render } from 'react-dom';
 import Card from './Card';
 
 interface HandProps {
   hand: Array<Card>;
 }
 
-const Hand = ({ hand }: HandProps): Array<Card> => {
-  render() {
+const Hand = ({ hand }: HandProps): JSX.Element => {
+  const listItems = hand.map((card) => {
+    const { value, color, cardType } = card.props;
+    return <Card value={value} color={color} cardType={cardType} />;
+  });
 
-    const listItems = hand.map((card) => {
-      const { value, color, cardType } = card.props;
-      <Card value={value} color={color} cardType={cardType} />
-    })
   return (
-    <>
-      <div
-        className="handContainer"
-        style={{ display: 'flex', flexWrap: 'wrap' }}
-      >
-      <h1>{listItems}</h1>
-      </div>
-    </>
+    <div
+      className="handContainer"
+      style={{ display: 'flex', flexWrap: 'wrap' }}
+    >
+      {listItems}
+    </div>
   );
-}
 };
 
 export default Hand;
