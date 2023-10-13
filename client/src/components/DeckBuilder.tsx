f05455c93d5bc0f0187b84c0e5b94b20fcbc8d0b
Ewelina Nicholson <e.guzikk@gmail.com>
2023-10-13 20:59:15 +0100

    TIC-71-addAvailableCardsSectionDeckBuilder

--- client/src/components/DeckBuilder.tsx
+++ client/src/components/DeckBuilder.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Card from './Card';
 import Header from './Header';
+import { useNavigate } from 'react-router-dom';
 
 function DeckBuilder() {
   const musicChoice = 'deckBuilder';
-
+  const navigate = useNavigate();
   const allCards = [
     {
       value: 1,
@@ -131,6 +132,19 @@ function DeckBuilder() {
   const [leftCards, setLeftCards] = useState(allCards);
   const [rightCards, setRightCards] = useState([]);
 
+  const clearChosenCards = () => {
+    setLeftCards(allCards);
+    setRightCards([]);
+  };
+  const startGame = useCallback(() => {
+    if (rightCards.length === 10) {
+      alert('Game will start with the chosen cards!');
+      navigate('/');
+    } else {
+      alert('Please select 10 cards to start the game!');
+    }
+  }, [rightCards, navigate]);
+
   const handleCardClick = (card) => {
     if (rightCards.length < 10 || card.selected) {
       const updatedLeftCards = leftCards.map((c) =>
@@ -151,29 +165,57 @@ function DeckBuilder() {
       <Header musicChoice={musicChoice} />
 
       <div className="deck-builder-container">
-        <div className="left-cards-container">
-          {leftCards.map((card, index) => (
-            <Card
-              key={index}
-              value={card.value}
-              color={card.color}
-              onClick={() => handleCardClick(card)}
-              selected={card.selected}
-              image={card.imagePath}
-            />
-          ))}
+        <div className="left-box">
+          <div className="left-text-container">
+            <p>Available Cards</p>
+          </div>
+
+          <div className="left-cards-container">
+            {leftCards.map((card, index) => (
+              <Card
+                key={index}
+                value={card.value}
+                color={card.color}
+                onClick={() => handleCardClick(card)}
+                selected={card.selected}
+                image={card.imagePath}
+                className="centered-card"
+              />
+            ))}
+          </div>
         </div>
-        <div className="right-cards-container">
-          {rightCards.map((card, index) => (
-            <Card
-              key={index}
-              value={card.value}
-              color={card.color}
-              onClick={() => handleCardClick(card)}
-              selected={true}
-              image={card.imagePath}
-            />
-          ))}
+
+        <div className="space-between"></div>
+
+        <div className="right-box">
+          <div className="right-text-container">
+            <p>Chosen Cards</p>
+          </div>
+          <div className="right-cards-container">
+            {rightCards.map((card, index) => (
+              <Card
+                key={index}
+                value={card.value}
+                color={card.color}
+                onClick={() => handleCardClick(card)}
+                selected={true}
+                image={card.imagePath}
+                className="centered-card"
+              />
+            ))}
+          </div>
+
+          <div className="button-container">
+            {/* Clear Chosen Cards Button */}
+            <button onClick={clearChosenCards} className="action-button">
+              Clear Chosen Cards
+            </button>
+
+            {/* Start Game Button */}
+            <button onClick={startGame} className="action-button">
+              Start Game
+            </button>
+          </div>
         </div>
       </div>
     </>
